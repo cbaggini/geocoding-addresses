ba98c7ba6de5b17e353b35208d7658af0b844ea5
Cecilia <earwen1986@gmail.com>
2020-10-12 14:52:38 +0100

    anonymised

--- django_project/asgi.py
+++ django_project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for staiy project.
+ASGI config for django_project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'staiy.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
 
 application = get_asgi_application()
--- django_project/urls.py
+++ django_project/urls.py
@@ -1,4 +1,4 @@
-"""staiy URL Configuration
+"""django_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
